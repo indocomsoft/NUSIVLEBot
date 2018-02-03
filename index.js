5b5896f77e224d4aef96fa69d08f8a1f186537eb
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-03 11:11:10 +0800

    Done MongoDB integration (#1)

---
+++ index.js
@@ -0,0 +1,17 @@
+// 3rd party libraries
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// My own libraries
+
+
+// Constants
+const MONGODB_SERVER = 'mongodb://localhost:27017';
+const DB_NAME = 'NUSIVLEBot';
+
+MongoClient.connect(MONGODB_SERVER, (err, client) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+  const db = client.db(DB_NAME);
+  client.close();
+});
