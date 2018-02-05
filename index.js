516cba8028394f21d7088001d705e6c3cb109742
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-02-05 12:26:51 +0800

    Connect to Telegram API only after MongoDB connection established

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
 } = process.env;
 const MONGODB_SERVER = 'mongodb://localhost:27017';
 const DB_NAME = 'NUSIVLEBot';
-const bot = new TelegramBot(TOKEN, { polling: true });
+let bot;
 let db;
 let chatId;
 
@@ -86,6 +86,23 @@ function recur(msg, modules, api) {
   };
 }
 
+function start(msg) {
+  bot.sendMessage(
+    msg.chat.id, `Hi ${msg.chat.first_name}! Let's get you set up!`,
+    {
+      parse_mode: 'Markdown',
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Get IVLE Token', url: `https://ivle.nus.edu.sg/api/login/?apikey=${API_KEY}&url=${URL_CALLBACK}` }],
+        ],
+      },
+    },
+  );
+  chatId.insert({ id: msg.chat.id, ivle_token: 'ask', chat: msg.chat });
+  return new Promise((resolve, reject) => reject());
+}
+
+
 MongoClient.connect(MONGODB_SERVER).then((client) => {
   console.log('Connected successfully to server');
   db = client.db(DB_NAME);
@@ -105,118 +122,102 @@ MongoClient.connect(MONGODB_SERVER).then((client) => {
   console.log(err);
   console.log('Unable to connect to server');
   process.exit(1);
-});
+}).then(() => {
+  bot = new TelegramBot(TOKEN, { polling: true });
+  bot.on('message', (msg) => {
+    const command = msg.text.split(' ')[0];
+    const args = msg.text.substr(command.length + 1);
+    let ready;
+    let modules;
 
-function start(msg) {
-  bot.sendMessage(
-    msg.chat.id, `Hi ${msg.chat.first_name}! Let's get you set up!`,
-    {
-      parse_mode: 'Markdown',
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Get IVLE Token', url: `https://ivle.nus.edu.sg/api/login/?apikey=${API_KEY}&url=${URL_CALLBACK}` }],
-        ],
-      },
-    },
-  );
-  chatId.insert({ id: msg.chat.id, ivle_token: 'ask', chat: msg.chat });
-  return new Promise((resolve, reject) => reject());
-}
-
-
-bot.on('message', (msg) => {
-  const command = msg.text.split(' ')[0];
-  const args = msg.text.substr(command.length + 1);
-  let ready;
-  let modules;
-
-  if (command !== '/delete') {
-    ready = chatId.findOne({ id: msg.chat.id }).then((r) => {
-      if (command === '/token') {
-        return createApi(msg.chat.id, args).catch(() => {
-          bot.sendMessage(msg.chat.id, 'Invalid token. Please try again.');
+    if (command !== '/delete') {
+      ready = chatId.findOne({ id: msg.chat.id }).then((r) => {
+        if (command === '/token') {
+          return createApi(msg.chat.id, args).catch(() => {
+            bot.sendMessage(msg.chat.id, 'Invalid token. Please try again.');
+            return start(msg);
+          }).then(() => {
+            bot.sendMessage(msg.chat.id, 'Token saved! Please run /setup to update your modules.');
+            return new Promise((resolve, reject) => reject(new Error('setup')));
+          });
+        } else if (r.ivle_token === 'ask') {
           return start(msg);
-        }).then(() => {
-          bot.sendMessage(msg.chat.id, 'Token saved! Please run /setup to update your modules.');
-          return new Promise((resolve, reject) => reject(new Error('setup')));
+        }
+        ({ modules } = r);
+        return createApi(msg.chat.id, r.ivle_token).catch(() => {
+          bot.sendMessage(msg.chat.id, 'Token has expired.');
+          return start(msg);
         });
-      } else if (r.ivle_token === 'ask') {
-        return start(msg);
-      }
-      ({ modules } = r);
-      return createApi(msg.chat.id, r.ivle_token).catch(() => {
-        bot.sendMessage(msg.chat.id, 'Token has expired.');
+      }).catch((err) => {
+        if (err.message === 'setup') {
+          return new Promise((resolve, reject) => reject(new Error('setup')));
+        }
         return start(msg);
       });
-    }).catch((err) => {
-      if (err.message === 'setup') {
-        return new Promise((resolve, reject) => reject(new Error('setup')));
-      }
-      return start(msg);
-    });
-  } else {
-    return chatId.deleteOne({ id: msg.chat.id }).then(() => {
-      bot.sendMessage(msg.chat.id, 'Your profile has been deleted.');
-    }).catch(() => {
-      bot.sendMessage(msg.chat.id, 'Failed to delete your profile. Please try again.');
-    });
-  }
-  return ready.then((api) => {
-    switch (command) {
-      case '/delete':
-        break;
-      case '/mods': {
-        let reply = '';
-        try {
-          modules.forEach((mod) => { reply = `${reply}\n- ${mod.CourseCode}: ${mod.CourseName}`; });
-        } catch (e) {
-          return bot.sendMessage(msg.chat.id, 'Please run /setup first');
-        }
-        bot.sendMessage(msg.chat.id, reply, { parse_mode: 'Markdown' });
-        break;
-      }
-      case '/setup':
-        api.do('Modules').then((response) => {
-          const result = response.Results.filter(mod => mod.isActive === 'Y')
-            .map(mod => ({ ID: mod.ID, CourseCode: mod.CourseCode, CourseName: mod.CourseName }));
-          return chatId.update({ id: msg.chat.id }, { $set: { modules: result } });
-        }).then(() => {
-          bot.sendMessage(msg.chat.id, 'Modules set up. Please run /push <on|off>');
-        }).catch(() => {
-          bot.sendMessage(msg.chat.id, 'Failed to setup, please run /setup again');
-        });
-        break;
-      case '/announcements':
-        fetchAnnouncements(msg, modules, api, true);
-        break;
-      case '/push': {
-        const status = args.split(' ')[0];
-        if (status === 'on') {
-          chatId.findOne({ id: msg.chat.id }).then((r) => {
-            if (!r.push) {
-              chatId.updateOne({ id: msg.chat.id }, { $set: { push: true } });
-              setTimeout(recur(msg, modules, api), parseInt(INTERVAL, 10) * 1000);
-              bot.sendMessage(msg.chat.id, `Set up announcements check every ${INTERVAL} seconds`);
-            } else {
-              bot.sendMessage(msg.chat.id, 'Push notification is already on');
-            }
-          });
-        } else {
-          chatId.updateOne({ id: msg.chat.id }, { $set: { push: false } });
-          bot.sendMessage(msg.chat.id, 'Turned off push notification');
-        }
-        break;
-      }
-      case '/help':
-        bot.sendMessage(msg.chat.id, 'Run /push <on|off>');
-        break;
-      case '/token':
-        break;
-      case '/start':
-        break;
-      default:
-        break;
+    } else {
+      return chatId.deleteOne({ id: msg.chat.id }).then(() => {
+        bot.sendMessage(msg.chat.id, 'Your profile has been deleted.');
+      }).catch(() => {
+        bot.sendMessage(msg.chat.id, 'Failed to delete your profile. Please try again.');
+      });
     }
-    return false;
+    return ready.then((api) => {
+      switch (command) {
+        case '/delete':
+          break;
+        case '/mods': {
+          let reply = '';
+          try {
+            modules.forEach((mod) => { reply = `${reply}\n- ${mod.CourseCode}: ${mod.CourseName}`; });
+          } catch (e) {
+            return bot.sendMessage(msg.chat.id, 'Please run /setup first');
+          }
+          bot.sendMessage(msg.chat.id, reply, { parse_mode: 'Markdown' });
+          break;
+        }
+        case '/setup':
+          api.do('Modules').then((response) => {
+            const result = response.Results.filter(mod => mod.isActive === 'Y')
+              .map(mod => ({ ID: mod.ID, CourseCode: mod.CourseCode, CourseName: mod.CourseName }));
+            return chatId.update({ id: msg.chat.id }, { $set: { modules: result } });
+          }).then(() => {
+            bot.sendMessage(msg.chat.id, 'Modules set up. Please run /push <on|off>');
+          }).catch(() => {
+            bot.sendMessage(msg.chat.id, 'Failed to setup, please run /setup again');
+          });
+          break;
+        case '/announcements':
+          fetchAnnouncements(msg, modules, api, true);
+          break;
+        case '/push': {
+          const status = args.split(' ')[0];
+          if (status === 'on') {
+            chatId.findOne({ id: msg.chat.id }).then((r) => {
+              if (!r.push) {
+                chatId.updateOne({ id: msg.chat.id }, { $set: { push: true } });
+                setTimeout(recur(msg, modules, api), parseInt(INTERVAL, 10) * 1000);
+                bot.sendMessage(msg.chat.id, `Set up announcements check every ${INTERVAL} seconds`);
+              } else {
+                bot.sendMessage(msg.chat.id, 'Push notification is already on');
+              }
+            });
+          } else {
+            chatId.updateOne({ id: msg.chat.id }, { $set: { push: false } });
+            bot.sendMessage(msg.chat.id, 'Turned off push notification');
+          }
+          break;
+        }
+        case '/help':
+          bot.sendMessage(msg.chat.id, 'Run /push <on|off>');
+          break;
+        case '/token':
+          break;
+        case '/start':
+          break;
+        default:
+          break;
+      }
+      return false;
+    });
   });
 });
